9c7bdded270867cd462fe17f1fa675160ca24b7d
nsordk <nicolai@nsor.dk>
2012-07-24 10:55:56 +0200

    v0.0.2 (regexJudge class added)

--- src/info/plugmania/regexecuter/Util.java
+++ src/info/plugmania/regexecuter/Util.java
@@ -1,10 +1,21 @@
 package info.plugmania.regexecuter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Util {
 	RegeXEcuter plugin;
 
 	public Util(RegeXEcuter instance) {
 		plugin = instance;
-	}	
+	}
 	
+	public boolean regexJudge(String str, String pattern){
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(str);
+		
+		if(m.find()) return true;
+		
+		return false;
+	}
 }
